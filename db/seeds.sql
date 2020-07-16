c1cbe77d99a7e36b8ec41dde7295b443add3fe23
Robert Luttig <robert.luttig@gmail.com>
2020-07-16 16:17:36 -0700

    added updated seed set up and created server connection

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,34 @@
--- INSERT INTO departments (depName) VALUES ("Sales"), ("Engineering");
--- INSERT INTO role (title, deptId, salary) VALUES ("Salesperson", 1, 80000), ("Engineer", 2, 100000);
--- INSERT INTO employee (firstName, lastName, roleId, manager_id)
- --   VALUES
-  --  ("John", "Doe", 1, 1),
-  --  ("Joe", "Rogan", 1, 2),
-   -- ("Pit", "Jupp", 2, 1),
-  --  ("Hansi", "Hintermeier", 2, 1);
+-- Departments
+INSERT INTO departments (depName) VALUES ("Sales");
+
+INSERT INTO departments (depName) VALUES ("Human Resource");
+
+INSERT INTO departments (depName) VALUES ("Engineering");
+
+
+-- Role
+INSERT INTO role (title, salary, deptId)
+VALUES ("Salesperson", 80000, 1);
+
+INSERT INTO role (title, salary, deptId)
+VALUES ("Engineer", 100000,3);
+
+INSERT INTO role (title, salary, deptId)
+VALUES ("HR Specialist", 60000,2);
+
+INSERT INTO role (title, salary, deptId)
+VALUES ("Lead Endineer", 150000,3);
+
+
+-- Employee
+INSERT INTO employee (firstName, lastName, roleId,manager_id)
+VALUES ("John", "Doe", 3, 1);
+
+INSERT INTO employee (firstName, lastName, roleId,manager_id)
+VALUES ("Joe", "Rogan", 1, 2);
+
+INSERT INTO employee (firstName, lastName, roleId,manager_id)
+VALUES ("Pit", "Jupp", 2, 1);
+
+INSERT INTO employee (firstName, lastName, roleId,manager_id)
+VALUES ("Hansi", "Hintermeier", 2, 1);
--- server.js
+++ server.js
@@ -1,3 +1,23 @@
+// import dependencies (mysql, inqurer,inqurer)
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 const cTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Password",
+    database: "management_db",
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.log("Unable to connect to data source. Good bye.");
+    } else {
+      mainMenu();
+    }
+  });
+  // List of constants for the different main menu action options
+    const VIEW_STUDENTS = "View Students";
+    const UPDATE_STUDENT_CLASS = "Update student class";
