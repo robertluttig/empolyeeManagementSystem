9bfe8d65d3a051e5c27d8e05f53a01218a151a4a
Robert Luttig <robert.luttig@gmail.com>
2020-07-16 19:56:48 -0700

    menu of user options  and switch board

--- server.js
+++ server.js
@@ -19,5 +19,50 @@ const connection = mysql.createConnection({
     }
   });
   // List of constants for the different main menu action options
-    const VIEW_STUDENTS = "View Students";
-    const UPDATE_STUDENT_CLASS = "Update student class";
+  function mainMenu() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "Add a new department",
+          "Add a new role",
+          "Add an employee",
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Update an employee's role",
+          "Exit",
+        ],
+      })
+      .then((answer) => {
+        switch (answer.action) {
+          case "Add a new department":
+            addNewDepartment();
+            break;
+        case "Add a new role":
+            addNewRole();
+            break;
+        case "Add an employee":
+                addEmployee();
+                break;
+        case "View all departments":
+            viewDepartments();
+            break;
+        case "View all roles":
+            viewAllRoles();
+            break;
+        case "View all employees":
+            viewAllEmployees();
+            break;
+        case "Update an employee's role":
+            updateEmployee();
+            break;
+        case "Exit":
+            connection.end();
+            break;
+        }
+      });
+
+    }
